52f5ab15696fc54977738974c35d60f671e699a1
logustra <logustra@gmail.com>
2022-03-14 15:04:41 +0700

    feat: auto/manual start, methods/events

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,60 @@
 <template>
-  <Countdown 
-    :time="time" 
-    v-slot="{
-      days,
-      hours,
-      minutes,
-      seconds
-    }"
-  >
-    {{ days }}d {{ hours }}h {{ minutes }}m {{ seconds }}s
-  </Countdown>
-  <!-- <Countdown 
-    :time="time" 
-    v-slot="{ foo }"
-  >
-   {{ foo }}
-  </Countdown> -->
+  <div>
+    <Countdown 
+      :auto="true"
+      :time="time" 
+      tag="div"
+      v-slot="{
+        isDone,
+        days,
+        hours,
+        minutes,
+        seconds
+      }"
+    >
+      {{ isDone ? 'Done!' : `${days}d ${hours}h ${minutes}m ${seconds}s` }}
+    </Countdown>
+
+    <Countdown 
+      :auto="auto"
+      :time="10000" 
+      tag="div"
+      @start="onStart"
+      @stop="onStop"
+      @done="onDone"
+      v-slot="{
+        isDone,
+        seconds
+      }"
+    >
+      {{ isDone ? 'Done!' : seconds }}
+    </Countdown>
+
+    <button @click="auto = true">
+      Start Countdown
+    </button>
+    <button @click="auto = false">
+      Stop Countdown
+    </button>
+  </div>
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue'
 import Countdown from './components/countdown.vue'
 
-// const time = new Date('Jan 5, 2024 15:37:25').getTime()
-const time = 600000
+const time = ref(new Date('Jan 5, 2024 15:37:25').getTime())
+const auto = ref(false)
+
+function onStart () {
+  console.log('onStart')
+}
+
+function onStop () {
+  console.log('onStop')
+}
+
+function onDone () {
+  console.log('onDone')
+}
 </script>
--- src/components/countdown.vue
+++ src/components/countdown.vue
@@ -5,12 +5,18 @@ import {
   ref,
   toRefs,
   computed,
-  onUnmounted,
+  watch,
+  onMounted,
 } from 'vue'
 
 export default defineComponent({
   name: 'Countdown',
   props: {
+    auto: {
+      type: Boolean,
+      required: false,
+      default: true
+    },
     tag: {
       type: String,
       required: false,
@@ -22,8 +28,9 @@ export default defineComponent({
       default: 0
     }
   },
-  setup(props, { slots }) {
+  setup(props, { emit, slots }) {
     const { 
+      auto,
       tag,
       time 
     } = toRefs(props)
@@ -39,16 +46,41 @@ export default defineComponent({
 
     /**
      * DES:
+     * event listeners
+     */
+    const EVENT_START = 'start'
+    const EVENT_STOP = 'stop'
+    const EVENT_DONE = 'done'
+
+    const isStart = ref(false)
+    const isStop = ref(false)
+    const isDone = ref(false)
+
+    /**
+     * DES:
      * get today's date and time
      */
-    const now = ref(Date.now())
+    const currentTime = ref(Date.now())
+    const startEndTime = ref(Date.now())
 
     /**
      * DES:
      * find the distance between now and the count down date
      */
-    const endTime = ref(Date.now() + time.value)
-    const distance = computed(() => endTime.value - now.value)
+    const endTime = computed(() => {
+      let result = time.value
+
+      /**
+       * DES:
+       * use this formula when time is less than 1 day
+       */
+      if (time.value < startEndTime.value) {
+        result = time.value + startEndTime.value
+      }
+
+      return result
+    })
+    const distance = computed(() => endTime.value - currentTime.value)
 
     /**
      * DES:
@@ -59,19 +91,73 @@ export default defineComponent({
     const minutes = computed(() => Math.floor((distance.value % HOURS) / MINUTES))
     const seconds = computed(() => Math.floor((distance.value % MINUTES) / SECONDS))
 
+    
+    function timeout (milliseconds: number) {
+      return new Promise(resolve => setTimeout(resolve, milliseconds))
+    }
+
+    function onStart() {
+      if (auto.value) {
+        currentTime.value = Date.now()
+        startEndTime.value = Date.now() - 1
+
+        /**
+         * DES:
+         * fires immediately when the countdown starts
+         */
+        isStart.value = true
+        emit(EVENT_START, isStart.value)
+      }
+    }
+
+    async function onUpdateCurrentTime () {
+      if (auto.value) {
+        if (distance.value > SECONDS) {
+          await timeout (SECONDS)
+          currentTime.value = Date.now()
+        } else {
+           /**
+           * DES:
+           * fires when the countdown has stopped
+           */
+          isDone.value = true
+          emit(EVENT_DONE, isDone.value)
+        }
+      } else {
+        /**
+         * DES:
+         * fires when the countdown has endded
+         */
+        isStart.value = false
+        isStop.value = true
+        emit(EVENT_STOP, isStop.value)
+      }
+    }
+
     /**
      * DES:
      * update the count down every 1 second
      */
-    const interval = setInterval(
+    watch(
+      () => currentTime.value,
       () => {
-        now.value = Date.now()
-      },
-      1000
+        onUpdateCurrentTime()
+      }
     )
 
-    onUnmounted(() => {
-      clearInterval(interval)
+    /**
+     * DES:
+     * fires immediately when the props auto changes
+     */
+    watch(
+      () => auto.value,
+      () => {
+        onStart()
+      }
+    )
+
+    onMounted(() => {
+      onStart()
     })
 
     /**
@@ -87,7 +173,10 @@ export default defineComponent({
         days: days.value,
         hours: hours.value,
         minutes: minutes.value,
-        seconds: seconds.value
+        seconds: seconds.value,
+        isStart: isStart.value,
+        isStop: isStop.value,
+        isDone: isDone.value
       })
     )
   } 
