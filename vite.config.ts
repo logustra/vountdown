e3759befcb9d50505e9ebeba7d6a0a983245e627
logustra <logustra@gmail.com>
2022-03-20 20:40:49 +0700

    feat: linter

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue';
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
